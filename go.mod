df57fcb34579212f1bfdec61176986f36bb743ff
lemon-mint <55233766+lemon-mint@users.noreply.github.com>
2021-01-06 10:13:50 +0900

    add /dump cmd

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.15
 require (
 	github.com/labstack/echo/v4 v4.1.17
 	github.com/lemon-mint/godotenv v1.0.1
+	github.com/patrickmn/go-cache v2.1.0+incompatible
 	golang.org/x/crypto v0.0.0-20200820211705-5c72a883971a
 	golang.org/x/net v0.0.0-20201016165138-7b1cca2348c0 // indirect
 )
--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"net/http"
 	"net/url"
 	"os"
@@ -14,10 +15,19 @@ import (
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 	"github.com/lemon-mint/godotenv"
+	"github.com/patrickmn/go-cache"
 	"golang.org/x/crypto/acme/autocert"
 )
 
 var apiBaseURL string
+
+type saystate struct {
+	ChatID int
+	UserID int
+	state  string
+}
+
+var statedb = cache.New(10*time.Minute, 3*time.Minute)
 
 func main() {
 	godotenv.Load()
@@ -48,14 +58,19 @@ func main() {
 				Command:     "/say",
 				Description: "/say <text>",
 			},
+			{
+				Command:     "/timeunix",
+				Description: "Displays the current Unix time.",
+			},
 		},
 	})
 	//fmt.Println(string(data))
-	http.Post(
+	resp, _ := http.Post(
 		apiBaseURL+"/setMyCommands",
 		"application/json",
 		bytes.NewReader(data),
 	)
+	ioutil.ReadAll(resp.Body)
 	e.Logger.Fatal(e.StartAutoTLS(":13230"))
 }
 
@@ -87,6 +102,13 @@ type webHookSendMessageResponse struct {
 	Text   string `json:"text"`
 }
 
+type webHookSendStyledMessageResponse struct {
+	Method string `json:"method"`
+	ChatID int    `json:"chat_id"`
+	Text   string `json:"text"`
+	Mode   string `json:"parse_mode"`
+}
+
 type webHookSendMessageReplyResponse struct {
 	Method  string `json:"method"`
 	ChatID  int    `json:"chat_id"`
@@ -107,6 +129,36 @@ func webhook(c echo.Context) error {
 	c.Bind(request)
 	fmt.Println(*request)
 	if hookid == "telegram" {
+		stateresp, ok := runState(request.Message.Text, *request)
+		if ok {
+			if stateresp.Type == "string" {
+				return c.JSON(200, webHookSendMessageResponse{
+					Method: "sendMessage",
+					ChatID: request.Message.Chat.ID,
+					Text:   stateresp.StringBody,
+				})
+			} else if stateresp.Type == "image" {
+				return c.JSON(200, webHookSendPhotoResponse{
+					Method: "sendPhoto",
+					ChatID: request.Message.Chat.ID,
+					Photo:  stateresp.ImageURL,
+				})
+			} else if stateresp.Type == "stringReply" {
+				return c.JSON(200, webHookSendMessageReplyResponse{
+					Method:  "sendMessage",
+					ChatID:  request.Message.Chat.ID,
+					Text:    stateresp.StringBody,
+					ReplyID: request.Message.MessageID,
+				})
+			} else if stateresp.Type == "stringstyle" {
+				return c.JSON(200, webHookSendStyledMessageResponse{
+					Method: "sendMessage",
+					ChatID: request.Message.Chat.ID,
+					Text:   stateresp.StringBody,
+					Mode:   stateresp.StyleFormat,
+				})
+			}
+		}
 		if strings.HasPrefix(request.Message.Text, "/") {
 			resp := runBotCmd(request.Message.Text, *request)
 			if resp.Type == "string" {
@@ -128,6 +180,13 @@ func webhook(c echo.Context) error {
 					Text:    resp.StringBody,
 					ReplyID: request.Message.MessageID,
 				})
+			} else if resp.Type == "stringstyle" {
+				return c.JSON(200, webHookSendStyledMessageResponse{
+					Method: "sendMessage",
+					ChatID: request.Message.Chat.ID,
+					Text:   resp.StringBody,
+					Mode:   resp.StyleFormat,
+				})
 			}
 		}
 	}
@@ -135,10 +194,11 @@ func webhook(c echo.Context) error {
 }
 
 type tgResponse struct {
-	Type       string
-	StringBody string
-	ImageURL   string
-	Body       interface{}
+	Type        string
+	StringBody  string
+	ImageURL    string
+	StyleFormat string
+	Body        interface{}
 }
 
 func runBotCmd(cmd string, ctx webhookEventRequest) tgResponse {
@@ -148,10 +208,17 @@ func runBotCmd(cmd string, ctx webhookEventRequest) tgResponse {
 			StringBody: "Hello, User!",
 		}
 	}
-	if cmd == "/ping" {
+	if cmd == "/timeunix" {
 		return tgResponse{
 			Type:       "string",
-			StringBody: "Pong!",
+			StringBody: strconv.Itoa(int(time.Now().UTC().Unix())),
+		}
+	}
+	if cmd == "/ping" {
+		return tgResponse{
+			Type:        "stringstyle",
+			StringBody:  "<i>Pong!</i>",
+			StyleFormat: "HTML",
 		}
 	}
 	if cmd == "/notrobot" {
@@ -170,6 +237,28 @@ func runBotCmd(cmd string, ctx webhookEventRequest) tgResponse {
 			StringBody: "Thank you for using!",
 		}
 	}
+	if cmd == "/say" {
+		go statedb.Set("saystate_"+strconv.Itoa(ctx.Message.Chat.ID), saystate{
+			ChatID: ctx.Message.Chat.ID,
+			UserID: ctx.Message.From.ID,
+			state:  "input",
+		}, cache.DefaultExpiration)
+		return tgResponse{
+			Type:       "string",
+			StringBody: "Please enter a sentence to say 😀",
+		}
+	}
+	if cmd == "/dump" {
+		go statedb.Set("_dump_"+strconv.Itoa(ctx.Message.Chat.ID), saystate{
+			ChatID: ctx.Message.Chat.ID,
+			UserID: ctx.Message.From.ID,
+			state:  "input",
+		}, cache.DefaultExpiration)
+		return tgResponse{
+			Type:       "string",
+			StringBody: "Please enter a sentence to dump 🍳",
+		}
+	}
 	if strings.HasPrefix(cmd, "/say ") && len(strings.SplitN(cmd, " ", 2)) == 2 {
 		return tgResponse{
 			Type:       "string",
@@ -185,5 +274,56 @@ func runBotCmd(cmd string, ctx webhookEventRequest) tgResponse {
 	return tgResponse{
 		Type:       "string",
 		StringBody: "Unknown Command\n.·´¯`(>▂<)´¯`·.",
+	}
+}
+
+func runState(text string, ctx webhookEventRequest) (tgResponse, bool) {
+	state, ok := statedb.Get("saystate_" + strconv.Itoa(ctx.Message.Chat.ID))
+	if ok {
+		if state.(saystate).state == "input" &&
+			state.(saystate).ChatID == ctx.Message.Chat.ID &&
+			state.(saystate).UserID == ctx.Message.From.ID {
+			statedb.Delete("saystate_" + strconv.Itoa(ctx.Message.Chat.ID))
+			go sendPlainMessage(ctx.Message.Chat.ID, text, int64(time.Millisecond*400), 0)
+			return tgResponse{
+				Type:       "string",
+				StringBody: "I understand, wait a moment 🤔",
+			}, true
+		}
+	}
+	state, ok = statedb.Get("_dump_" + strconv.Itoa(ctx.Message.Chat.ID))
+	if ok {
+		if state.(saystate).state == "input" &&
+			state.(saystate).ChatID == ctx.Message.Chat.ID &&
+			state.(saystate).UserID == ctx.Message.From.ID {
+			statedb.Delete("_dump_" + strconv.Itoa(ctx.Message.Chat.ID))
+			data, _ := json.Marshal(ctx)
+			return tgResponse{
+				Type:       "string",
+				StringBody: string(data),
+			}, true
+		}
+	}
+	return tgResponse{}, false
+}
+
+func sendPlainMessage(chatID int, msg string, delay int64, postDelay int64) {
+	jsondata, err := json.Marshal(
+		struct {
+			ChatID int    `json:"chat_id"`
+			Text   string `json:"text"`
+		}{
+			ChatID: chatID,
+			Text:   msg,
+		},
+	)
+	if err == nil {
+		if delay != 0 {
+			time.Sleep(time.Duration(delay))
+		}
+		http.Post(apiBaseURL+"/sendMessage", "application/json", bytes.NewReader(jsondata))
+		if postDelay != 0 {
+			time.Sleep(time.Duration(postDelay))
+		}
 	}
 }
